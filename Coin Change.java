e42151ed4924ca0825d84d4a1b18d02aea1c20ea
Shivesh <122783487+ShiveshK09@users.noreply.github.com>
2026-09-03 18:06:53 +0530

    Add coin change solution using dynamic programming
    Implement dynamic programming solution for coin change problem.

---
+++ Coin Change.java
@@ -0,0 +1,21 @@
+//https://leetcode.com/problems/coin-change/
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, amount + 1);
+        dp[0] = 0;
+        for (int i = 1; i <= amount; i++)
+            for (int coin : coins)
+                if (i >= coin)
+                    dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+        return dp[amount] > amount ? -1 : dp[amount];
+    }
+}
+/*
+Pattern: CURRENT → REMAINING + 1 → MIN
+dp[0] = 0
+fill = INF
+for amount
+    for coin
+        dp[i] = min(dp[i], dp[i-coin] + 1)
+*/
