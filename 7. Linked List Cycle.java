e8855fe28190c336e6ad88867a8f6d5e88394887
Shivesh <122783487+ShiveshK09@users.noreply.github.com>
2026-09-06 03:07:20 +0530

    Add cycle detection method for linked list
    Implement the hasCycle method to detect a cycle in a linked list.

---
+++ 7. Linked List Cycle.java
@@ -0,0 +1,15 @@
+//https://leetcode.com/problems/linked-list-cycle/
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        if(head==null || head.next==null) return false;
+        ListNode slow=head;
+        ListNode fast=head;
+        while(fast!=null && fast.next!=null){
+            if(fast==null && fast.next==null) return false;
+            slow=slow.next;
+            fast=fast.next.next;
+            if(slow==fast) return true;
+        }
+        return false;
+    }
+}
